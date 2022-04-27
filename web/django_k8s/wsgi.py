fc1ddfe680d72e0defd61122e9117b2b07f1acf6
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-28 01:30:10 +0300

    docker setup

--- web/django_k8s/wsgi.py
+++ web/django_k8s/wsgi.py
@@ -1,8 +1,6 @@
 """
 WSGI config for django_k8s project.
-
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
@@ -13,8 +11,6 @@ import pathlib
 import dotenv
 
 from django.core.wsgi import get_wsgi_application
-
-from web.django_k8s.settings import BASE_DIR
 
 CURRENT_DIR = pathlib.Path(__file__).resolve().parent
 BASE_DIR = CURRENT_DIR.parent
