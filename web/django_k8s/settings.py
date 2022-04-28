1ceb84df4154bbe43e8ffeed015b74dad577483f
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-28 13:18:32 +0300

    docker-compose

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -93,10 +93,7 @@ DB_IS_AVIAL = all([
     DB_PORT
 ])
 
-POSTGRES_READY = str(os.environ.get("POSTGRES_READY")) == "1"
-
-
-if DB_IS_AVIAL and POSTGRES_READY:
+if DB_IS_AVIAL:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
